5107ef92639fe031693a436952eaa1e8572aae0c
Patrik Kováč <58639822+Patrik-Kovac@users.noreply.github.com>
2022-12-22 18:26:12 +0100

    Connected to a webservice using retrofit

--- app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -2,3 +2,21 @@
 package com.example.android.marsrealestate.network
 
 private const val BASE_URL = "https://android-kotlin-fun-mars-server.appspot.com/"
+
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(ScalarsConverterFactory.create())
+    .baseUrl(BASE_URL)
+    .build()
+
+interface MarsApiService {
+    @GET("realestate")
+    fun getProperties():
+            Call<String>
+}
+object MarsApi {
+    val retrofitService : MarsApiService by lazy {
+        retrofit.create(MarsApiService::class.java) }
+}
